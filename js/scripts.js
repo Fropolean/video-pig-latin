29db66e5c31cea6e618204fe6e04a646eba42501
Keir Staple and Marika Allely <mballely@gmail.com>
2016-01-20 15:19:38 -0800

    add nested for loops for unlimited consonants

--- js/scripts.js
+++ js/scripts.js
@@ -5,22 +5,43 @@
 //   return array.toString();
 // }
 
-var pigLatin = function(userInput) {
-  if (userInput[0] === "a" || userInput[0] === "e" || userInput[0] === "i" || userInput[0] === "o" || userInput[0] === "u") {
-    return userInput + "way"; // if first letter is vowel
 
-  } else if (userInput[2] !== "a" && userInput[2] !== "e" && userInput[2] !== "i" && userInput[2] !== "o" && userInput[2] !== "u" && userInput[2] !== "y") {
-    return userInput.slice(3) + userInput[0] + userInput[1] + userInput[2] + "ay"; //if first three letters are consonants
+//START OF FUNCTIONING CODE
+// var pigLatin = function(userInput) {
+//   if (userInput[0] === "a" || userInput[0] === "e" || userInput[0] === "i" || userInput[0] === "o" || userInput[0] === "u") {
+//     return userInput + "way"; // if first letter is vowel
+//
+//   } else if (userInput[2] !== "a" && userInput[2] !== "e" && userInput[2] !== "i" && userInput[2] !== "o" && userInput[2] !== "u" && userInput[2] !== "y") {
+//     return userInput.slice(3) + userInput[0] + userInput[1] + userInput[2] + "ay"; //if first three letters are consonants
+//
+//   } else if (userInput[1] !== "a" && userInput[1] !== "e" && userInput[1] !== "i" && userInput[1] !== "o" && userInput[1] !== "u" && userInput[1] !== "y") {
+//     return userInput.slice(2) + userInput[0] + userInput[1] + "ay"; //if first two letters are consonants
+//
+//   } else { //if word begins with consonant
+//     return userInput.slice(1) + userInput[0] + "ay";
+//   }
+// }
+//END OF FUNCTIONING CODE
 
-  } else if (userInput[1] !== "a" && userInput[1] !== "e" && userInput[1] !== "i" && userInput[1] !== "o" && userInput[1] !== "u" && userInput[1] !== "y") {
-    return userInput.slice(2) + userInput[0] + userInput[1] + "ay"; //if first two letters are consonants
 
-  } else { //if word begins with consonant
-    return userInput.slice(1) + userInput[0] + "ay";
+var english = function(userInput) {
+
+  var vowels = ["a", "e", "i", "o", "u", " "];
+  var letters = userInput.split("");
+
+  for (var i = 0; i < letters.length; i++) {
+    for (var j = 0; j < vowels.length; j++) {
+      if(letters[i] === vowels[j]) {
+        return i;
+      }
+    }
   }
 }
 
-
+var pigLatin = function(userInput) {
+  var splitAt = english(userInput);
+  return userInput.slice(splitAt) + userInput.slice(0, splitAt) + "ay";
+}
 
 $(document).ready(function() {
   $("form#userForm").submit(function(event) {
--- specs/specs.js
+++ specs/specs.js
@@ -2,6 +2,4 @@ describe ("pigLatin", function() {
   it("is false for words that start with vowels", function(){
     expect(pigLatin("alphabet")).to.equal(false);
   });
-
-
 });
