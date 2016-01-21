929d56edeaa8bfc9c63961837cddfea8f83050b9
Keir Staple and Marika Allely <student@epicodus.com>
2016-01-20 18:05:05 -0800

    save before computers shut off

--- js/scripts.js
+++ js/scripts.js
@@ -1,60 +1,21 @@
-// var pigLatin = function(userInput) {
-//   var array = userInput.split("");
-//   var remove = array.shift();
-//   array.push(remove, "ay");
-//   return array.toString();
-// }
+var pigLatin = function(englishWord) {
+  if (hasVowelAt(englishWord, 0)) {
+    var translatedWord = englishWord;
+  } else if (!hasVowelAt(englishWord, 1)) {
+    var translatedWord = englishWord.slice(1) + englishWord.slice(0, 1);
+  } else if (!hasVowelAt(englishWord, 2)) {
+    var translatedWord = englishWord.slice(2) + englishWord.slice(0, 2);
+  }
+  return translatedWord + "ay";
+};
 
-
-//START OF FUNCTIONING CODE
-// var pigLatin = function(userInput) {
-//   if (userInput[0] === "a" || userInput[0] === "e" || userInput[0] === "i" || userInput[0] === "o" || userInput[0] === "u") {
-//     return userInput + "way"; // if first letter is vowel
-//
-//   } else if (userInput[2] !== "a" && userInput[2] !== "e" && userInput[2] !== "i" && userInput[2] !== "o" && userInput[2] !== "u" && userInput[2] !== "y") {
-//     return userInput.slice(3) + userInput[0] + userInput[1] + userInput[2] + "ay"; //if first three letters are consonants
-//
-//   } else if (userInput[1] !== "a" && userInput[1] !== "e" && userInput[1] !== "i" && userInput[1] !== "o" && userInput[1] !== "u" && userInput[1] !== "y") {
-//     return userInput.slice(2) + userInput[0] + userInput[1] + "ay"; //if first two letters are consonants
-//
-//   } else { //if word begins with consonant
-//     return userInput.slice(1) + userInput[0] + "ay";
-//   }
-// }
-//END OF FUNCTIONING CODE
-
-
-var english = function(userInput) {
-
-  var vowels = ["a", "e", "i", "o", "u", " "];
-  var letters = userInput.split("");
-
-  for (var i = 0; i < letters.length; i++) {
-    for (var j = 0; j < vowels.length; j++) {
-      if(letters[i] === vowels[j]) {
-        return i;
-      }
+var hasConsonantAt = function(word, position) {
+  var vowels = ["a", "e", "i", "o", "u"];
+  for (var i = 0; i < vowels.length; i++) {
+    if (word[position] === vowels[i]) {
+      return true;
+      break;
     }
   }
+  return false;
 }
-
-var pigLatin = function(userInput) {
-  var splitAt = english(userInput);
-  return userInput.slice(splitAt) + userInput.slice(0, splitAt) + "ay";
-}
-
-$(document).ready(function() {
-  $("form#userForm").submit(function(event) {
-
-    var userInput = $("textarea#engInput").val();
-    var userInput = userInput.toLowerCase();
-    var result = pigLatin(userInput);
-
-    // console.log(userInput);
-    // console.log(result);
-
-  $("#output").text(result);
-  $("#result").show();
-  event.preventDefault();
-  });
-});
--- specs/specs.js
+++ specs/specs.js
@@ -1,5 +1,28 @@
 describe ("pigLatin", function() {
-  it("is false for words that start with vowels", function(){
-    expect(pigLatin("alphabet")).to.equal(false);
+  it("add 'ay' to the end of a word that starts with a vowel", function(){
+    expect(pigLatin("apple")).to.equal("appleay");
+  });
+
+  it("adds 'ay' to the end of a word that starts with any vowel", function() {
+    expect(pigLatin("english")).to.equal("englishay");
+  })
+
+  it("takes a single consonant at the beginning of a word and moves it to the end", function() {
+    expect(pigLatin("cat")).to.equal("atcay");
+  });
+
+  it("takes two consonants at the beginning of a word and moves then to the end", function() {
+    expect(pigLatin("cricket")).to.equal("icketcray");
+  });
+});
+
+
+describe("hasConsonantAt", function() {
+  it("returns true if a word has a consonant in the specified spot", function() {
+    expect(hasConsonantAt("peanut", 1)).to.equal(true);
+  });
+
+  it("returns false if a word does not have a consonant in the specified spot", function() {
+    expect(hasConsonantAt("peanut", 1)).to.equal(false);
   });
 });
